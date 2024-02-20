ed39636e65b0699e90dd2b6edcd966416bdf8594
shifc <shifc@intest.cn>
2024-02-20 09:58:39 +0800

    test：开发登录主界面。 date：20240220

---
+++ app/src/main/java/com/example/xugang__project/view/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.xugang__project.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.xugang__project.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
